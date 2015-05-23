82ccb5de4225dc142d23fc6bf4f86c7112416ad3
jayli <knuth.fan@gmail.com>
2015-05-23 20:45:22 +0800

    修改

--- Tencentyun/Http.php
+++ Tencentyun/Http.php
@@ -47,7 +47,7 @@ class Http
             curl_setopt($curlHandle, CURLOPT_CAINFO, $rq['cert']);
             curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST,2);
             isset($rq['ssl_version']) && curl_setopt($curlHandle, CURLOPT_SSLVERSION, $rq['ssl_version']);
-        }else if( $ssl && !$cert ){
+        }else if( $ssl ){
             curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER,false);   //true any ca
             curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST,1);       //check only host
             isset($rq['ssl_version']) && curl_setopt($curlHandle, CURLOPT_SSLVERSION, $rq['ssl_version']);
