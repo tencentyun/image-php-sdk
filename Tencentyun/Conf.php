4bd59fe4829dd2d997b0253fb18bd84028999d8f
moria <knuth.fan@gmail.com>
2015-07-21 20:18:32 +0800

    增加注释

--- Tencentyun/Conf.php
+++ Tencentyun/Conf.php
@@ -11,11 +11,15 @@ class Conf
 
 	const API_VIDEO_END_POINT = 'http://web.video.myqcloud.com/videos/v1/';
 		
+    // 以下部分请您根据在qcloud申请到的项目id和对应的secret id和secret key进行修改
+
     const APPID = '10000002';
 
     const SECRET_ID = 'AKIDL5iZVplWMenB5Zrx47X78mnCM3F5xDbC';
 
     const SECRET_KEY = 'Lraz7n2vNcyW3tiP646xYdfr5KBV4YAv';
+
+    // 以上部分请您根据在qcloud申请到的项目id和对应的secret id和secret key进行修改
 
     public static function getUA() {
         return 'QcloudPHP/'.self::PKG_VERSION.' ('.php_uname().')';
